52317668aff5870909b0686ec71dc06d1d35d260
jakir7890 <syedjakir7890@gamil.com>
2022-10-30 13:19:26 +0600

    validated

--- lib/bottom_pages/my_adds.dart
+++ lib/bottom_pages/my_adds.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class MyAddScreen extends StatelessWidget {
-
+// GlobalKey formKey =GlobalKey<>()
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            //  key: ,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {},
+                  decoration: InputDecoration(
+                      hintText: 'Enter Your E-mail W',
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.redAccent,
+                            width: 3,
+                          ))),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'Enter your Password',
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.redAccent,
+                            width: 3,
+                          ))),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Enter W", style: GoogleFonts.raleway(fontSize: 20, fontWeight: FontWeight.w900),)),
+                SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
