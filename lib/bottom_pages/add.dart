65d9411620621a6b2ca38170d02136ea0b57bdeb
jakir7890 <syedjakir7890@gamil.com>
2022-10-31 15:46:55 +0600

    hive add

--- lib/bottom_pages/add.dart
+++ lib/bottom_pages/add.dart
@@ -8,23 +8,29 @@ class AddScreen extends StatefulWidget {
 }
 
 class _AddScreenState extends State<AddScreen> {
+
+// @override
+// void initState() {
+//   super.initState();
+//   _updateController =  contactBox!.getAt(widget.);
+// }
+
+
   TextEditingController _controller = TextEditingController();
+  TextEditingController _updateController = TextEditingController(text: 'Hello');
 
   Box? contactBox;
-  @override
-  void initState() {
-    contactBox = Hive.box('contact-box');
-    super.initState();
-  }
+ 
+
 
   @override
   Widget build(BuildContext context) {
+    
     return SafeArea(
       child: Scaffold(
-        body: 
-        GestureDetector(
+        body: GestureDetector(
           onTap: () {
-            FocusManager.instance.primaryFocus!.unfocus()
+            FocusManager.instance.primaryFocus!.unfocus();
           },
           child: Padding(
             padding: const EdgeInsets.all(20),
@@ -33,7 +39,9 @@ class _AddScreenState extends State<AddScreen> {
                 TextFormField(
                   controller: _controller,
                   decoration: InputDecoration(
-                      hintText: 'Enter your Data', border: OutlineInputBorder()),
+                    
+                      hintText: 'Enter your Data',
+                      border: OutlineInputBorder()),
                 ),
                 SizedBox(
                   width: 250,
@@ -60,6 +68,88 @@ class _AddScreenState extends State<AddScreen> {
                         return Card(
                           child: ListTile(
                             title: Text(contactBox!.getAt(index)),
+                            trailing: SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  IconButton(
+                                      tooltip: 'Edit your data',
+                                      onPressed: () async {
+                                        showDialog(
+                                            context: context,
+                                            builder: (_) {
+                                              return Dialog(
+                                                child: SizedBox(
+                                                  height: 200,
+                                                  child: Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child: Column(
+                                                      children: [
+                                                        TextFormField(
+                                                         
+                                                          controller:
+                                                              _updateController,
+                                                          decoration: InputDecoration(
+                                                              border: OutlineInputBorder(
+                                                                  borderRadius:
+                                                                      BorderRadius
+                                                                          .circular(
+                                                                              10))),
+                                                        ),
+                                                        SizedBox(
+                                                          width: 200,
+                                                          child: ElevatedButton(
+                                                              onPressed:
+                                                                  () async {
+                                                                final updateData =
+                                                                    _updateController
+                                                                        .text;
+                                                                contactBox!.putAt(
+                                                                    index,
+                                                                    updateData);
+                                                                _updateController
+                                                                    .clear();
+                                                                Navigator.pop(
+                                                                    context);
+                                                                Fluttertoast
+                                                                    .showToast(
+                                                                        msg:
+                                                                            "Updated Data Successfully");
+                                                              },
+                                                              child: Text(
+                                                                  'Upadate Data')),
+                                                        )
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                              );
+                                            });
+                                      },
+                                      icon: Icon(
+                                        Icons.edit,
+                                        color: Colors.blue,
+                                        size: 20,
+                                      )),
+                                  IconButton(
+                                      color: Colors.red,
+                                      tooltip: 'Edit your data',
+                                      onPressed: () async {
+                                        await contactBox!.deleteAt(index);
+                                        Fluttertoast.showToast(
+                                            msg: 'Deleted Successfully');
+                                      },
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.redAccent,
+                                        size: 20,
+                                      ))
+                                ],
+                              ),
+                            ),
                           ),
                         );
                       },
