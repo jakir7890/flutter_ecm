bd21e19226ca38ee5fb9139f9a4ac00623dd4c8a
jakir7890 <syedjakir7890@gamil.com>
2022-10-31 10:46:58 +0600

    ss add

--- lib/bottom_pages/add.dart
+++ lib/bottom_pages/add.dart
@@ -21,47 +21,53 @@ class _AddScreenState extends State<AddScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(
-                    hintText: 'Enter your Data', border: OutlineInputBorder()),
-              ),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        final inputValue = _controller.text;
-                        await contactBox!.add(inputValue);
-                        Fluttertoast.showToast(msg: 'Added Succesfully');
-                        _controller.clear();
-                      } catch (e) {
-                        Fluttertoast.showToast(msg: e.toString());
-                      }
-                    },
-                    child: Text('Add Data')),
-              ),
-              Expanded(
-                  child: ValueListenableBuilder(
-                valueListenable: Hive.box('contact-box').listenable(),
-                builder: ((context, value, child) {
-                  return ListView.builder(
-                    itemCount: contactBox!.keys.toList().length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(contactBox!.getAt(index)),
-                        ),
-                      );
-                    },
-                  );
-                }),
-              ))
-            ],
+        body: 
+        GestureDetector(
+          onTap: () {
+            FocusManager.instance.primaryFocus!.unfocus()
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                      hintText: 'Enter your Data', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          final inputValue = _controller.text;
+                          await contactBox!.add(inputValue);
+                          Fluttertoast.showToast(msg: 'Added Succesfully');
+                          _controller.clear();
+                        } catch (e) {
+                          Fluttertoast.showToast(msg: e.toString());
+                        }
+                      },
+                      child: Text('Add Data')),
+                ),
+                Expanded(
+                    child: ValueListenableBuilder(
+                  valueListenable: Hive.box('contact-box').listenable(),
+                  builder: ((context, value, child) {
+                    return ListView.builder(
+                      itemCount: contactBox!.keys.toList().length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            title: Text(contactBox!.getAt(index)),
+                          ),
+                        );
+                      },
+                    );
+                  }),
+                ))
+              ],
+            ),
           ),
         ),
       ),
